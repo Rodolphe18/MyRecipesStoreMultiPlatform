4f22db757beace4fe03af2346ccdce261aa03768
rodolphe <rodolphefrancotte18@gmail.com>
2026-06-15 20:49:49 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyRecipesStoreKmp"
+include(":androidApp")
+include(":shared")
+include(":core:model")
